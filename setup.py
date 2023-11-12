22c1b91fec10631e7277450ba8ee4a0b195630d8
Gustavo Arango <gaarangoa@gmail.com>
2023-11-11 19:24:26 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DeepARG',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(
         exclude=(".git", "data")
     ),
